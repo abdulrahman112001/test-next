45b0968e42bf10b66589668a27ddb7cceaf8fd75
abdulrahman112001 <abdulrahmanelshikh2001@gmail.com>
2023-08-04 19:27:47 +0300

    psuh

---
+++ pages/Blogs/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetailsPlg() {
+  return (
+    <div>
+        DetailsPlg test
+    </div>
+  )
+}
---
+++ pages/Blogs/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <div>
+
+
+      <Link href={`Blogs/2`}>details</Link>
+    </div>
+  )
+}
